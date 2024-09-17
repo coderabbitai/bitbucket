d5821cf20cec35194517606b5026658645ae82bd
Nathan Gendron <nato@coderabbit.ai>
2024-09-17 16:46:02 -0400

    🎉 Initial commit

---
+++ tests/env.ts
@@ -0,0 +1,78 @@
+import { config } from "dotenv"
+import path from "path"
+
+/**
+ * @see https://nodejs.org/en/learn/getting-started/nodejs-the-difference-between-development-and-production
+ * @see https://vitest.dev/guide/migration.html#envs
+ */
+export type NodeEnv = (typeof nodeEnvs)[keyof typeof nodeEnvs]
+export type ProcessEnv = typeof process.env
+
+export function isNodeEnv(value: unknown): value is NodeEnv {
+	return Object.values<unknown>(nodeEnvs).includes(value)
+}
+
+export function toNodeEnv(value: unknown): NodeEnv {
+	if (isNodeEnv(value)) return value
+	return nodeEnvs.development
+}
+
+const nodeEnvs = {
+	development: "development",
+	production: "production",
+	/**
+	 * Vitest sets `NODE_ENV` to `test` if it wasn't set before.
+	 * @see https://vitest.dev/guide/migration.html#envs
+	 */
+	test: "test",
+} as const
+
+interface LoadedEnv extends ProcessEnv {
+	readonly NODE_ENV: NodeEnv
+}
+
+/** Loads environment variables from the `.env` files. `NODE_ENV` has to be
+ * set in the environment and will not be picked up from there.
+ *
+ * If `NODE_ENV` is not set, it will default to `development`.
+ *
+ * Environment variables are loaded in the following order:
+ *
+ * 1. `.env.development.local`
+ * 2. `.env.development`
+ * 3. `.env.local`
+ * 4. `.env`
+ */
+function loadEnv(): LoadedEnv {
+	const cwd = process.cwd()
+	const NODE_ENV = toNodeEnv(process.env.NODE_ENV?.trim())
+
+	const { parsed, error } = config({
+		path: [
+			path.resolve(cwd, `.env.${NODE_ENV}.local`),
+			path.resolve(cwd, `.env.${NODE_ENV}`),
+			path.resolve(cwd, ".env.local"),
+			path.resolve(cwd, ".env"),
+		],
+	})
+
+	if (!parsed)
+		throw new Error("Environment variables could not be loaded.", {
+			cause: error,
+		})
+
+	const merged = Object.assign(parsed, process.env, { NODE_ENV })
+	process.env = merged
+	return merged
+}
+
+const parsed = loadEnv()
+
+function envString(key: string) {
+	const value = parsed[key]
+	if (!value) throw new Error(`$${key} is missing`)
+	return value
+}
+
+export const BITBUCKET_CLOUD_URL = envString("BITBUCKET_CLOUD_URL")
+export const BITBUCKET_CLOUD_TOKEN = envString("BITBUCKET_CLOUD_TOKEN")
