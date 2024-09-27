02b07c9e18764766e125fa9cf6b163b70471b5db
Nathan Gendron <nato@coderabbit.ai>
2024-09-27 15:33:50 -0400

    🔨 Make sure we're on main in pnpm_version_patch.sh

--- scripts/pnpm_version_patch.sh
+++ scripts/pnpm_version_patch.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+git checkout main
+git pull --autostash --prune --rebase
+
 VERSION=$(pnpm version patch --no-git-tag-version)
+
+pnpm i
+pnpm lint:fix
+
+git status
 
 echo 'Do you want to publish @coderabbitai/bitbucket@'$VERSION'? (y/n) '
 read Y
