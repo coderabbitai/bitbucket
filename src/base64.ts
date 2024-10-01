7a43f01a5034c3ecc4f57dadcb910fc692a646f9
Nathan Gendron <nato@coderabbit.ai>
2024-10-01 13:21:26 -0400

    🎨 Run tsco

--- src/base64.ts
+++ src/base64.ts
@@ -1,11 +1,11 @@
-export function toBase64(input: string): string {
-	const bytes = new TextEncoder().encode(input)
-	const string = String.fromCodePoint(...bytes)
-	return btoa(string)
-}
-
 export function fromBase64(base64: string): Buffer | Uint8Array | string {
 	const string = atob(base64)
 	const bytes = Uint8Array.from(string, v => v.codePointAt(0) ?? 0)
 	return new TextDecoder().decode(bytes)
+}
+
+export function toBase64(input: string): string {
+	const bytes = new TextEncoder().encode(input)
+	const string = String.fromCodePoint(...bytes)
+	return btoa(string)
 }
