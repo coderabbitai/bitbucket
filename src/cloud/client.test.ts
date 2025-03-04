aca662039dbbe04a7f2824e1dfca2eb2bd9af551
Nathan Gendron <nato@coderabbit.ai>
2025-03-04 13:23:00 -0500

    🏷️ Override Bitbucket Cloud's schema to create new branches

--- src/cloud/client.test.ts
+++ src/cloud/client.test.ts
@@ -1,7 +1,20 @@
 import { test } from "vitest"
 import { createBitbucketCloudClient } from "./client.ts"
 
-test("createBitbucketCloudClient", ({ expect }) => {
+test("createBitbucketCloudClient", async ({ expect }) => {
 	const client = createBitbucketCloudClient()
 	expect(client).toBeDefined()
+
+	await client.POST("/repositories/{workspace}/{repo_slug}/refs/branches", {
+		params: {
+			path: {
+				repo_slug: "repo_slug",
+				workspace: "workspace",
+			},
+		},
+		body: {
+			name: "name",
+			target: { hash: "hash" },
+		},
+	})
 })
--- src/cloud/index.ts
+++ src/cloud/index.ts
@@ -1,2 +1,3 @@
 export * from "./client.ts"
+export type * from "./interfaces/index.ts"
 export type * as OpenApi from "./openapi/index.ts"
