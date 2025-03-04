97a59c7cefbf6cb4a381f6a51b0a300bfe373e67
Nathan Gendron <nato@coderabbit.ai>
2025-03-04 13:26:00 -0500

    🔥 Remove test accidentally left in

--- src/cloud/client.test.ts
+++ src/cloud/client.test.ts
@@ -1,20 +1,7 @@
 import { test } from "vitest"
 import { createBitbucketCloudClient } from "./client.ts"
 
-test("createBitbucketCloudClient", async ({ expect }) => {
+test("createBitbucketCloudClient", ({ expect }) => {
 	const client = createBitbucketCloudClient()
 	expect(client).toBeDefined()
-
-	await client.POST("/repositories/{workspace}/{repo_slug}/refs/branches", {
-		params: {
-			path: {
-				repo_slug: "repo_slug",
-				workspace: "workspace",
-			},
-		},
-		body: {
-			name: "name",
-			target: { hash: "hash" },
-		},
-	})
 })
