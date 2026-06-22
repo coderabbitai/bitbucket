5e9640a865e786c8cb1f6a465fc675ea5ce65b85
Nathan Gendron <nato@coderabbit.ai>
2026-06-22 15:21:43 -0400

    🥅 Make the generation pipeline fail fast on command and HTTP errors.
    Signed-off-by: Nathan Gendron <nato@coderabbit.ai>

--- scripts/openapi.sh
+++ scripts/openapi.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 # Bitbucket Cloud
-curl --output ./src/cloud/openapi/swagger.v3.json https://dac-static.atlassian.com/cloud/bitbucket/swagger.v3.json
-pnpm exec openapi-typescript ./src/cloud/openapi/swagger.v3.json --output ./src/cloud/openapi/openapi-typescript.ts --immutable --empty-objects-unknown --alphabetize --root-types
-pnpm exec ts-autofix ./src/cloud/openapi
+cloud() {
+	curl --fail --location --output ./src/cloud/openapi/swagger.v3.json --show-error --silent https://dac-static.atlassian.com/cloud/bitbucket/swagger.v3.json
+	pnpm exec openapi-typescript ./src/cloud/openapi/swagger.v3.json --output ./src/cloud/openapi/openapi-typescript.ts --immutable --empty-objects-unknown --alphabetize --root-types
+	pnpm exec ts-autofix ./src/cloud/openapi
+}
 
 # Bitbucket Data Center
-curl --output ./src/server/openapi/swagger.v3.json https://dac-static.atlassian.com/server/bitbucket/9.2.swagger.v3.json
-pnpm exec openapi-typescript ./src/server/openapi/swagger.v3.json --output ./src/server/openapi/openapi-typescript.ts --immutable --empty-objects-unknown --alphabetize --root-types
-pnpm exec ts-autofix ./src/server/openapi
+server() {
+	curl --fail --location --output ./src/server/openapi/swagger.v3.json --show-error --silent https://dac-static.atlassian.com/server/bitbucket/9.2.swagger.v3.json
+	pnpm exec openapi-typescript ./src/server/openapi/swagger.v3.json --output ./src/server/openapi/openapi-typescript.ts --immutable --empty-objects-unknown --alphabetize --root-types
+	pnpm exec ts-autofix ./src/server/openapi
+}
+
+cloud &
+server &
+wait
 
 # Use `operationId` as method names
 node ./scripts/generate_clients.ts
 pnpm run lint:fix
+git status
